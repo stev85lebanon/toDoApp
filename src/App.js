8bd8b04981efaa8598646ec1ba310175f471da3e
stev85lebanon <mostafaljamal@outlook.com>
2024-08-25 14:17:57 +0200

    first commit todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import "./App.css"
+const App = () => {
+  const [todo,setTodo]=useState([])
+const inputRef=useRef()
 
-function App() {
+const handleAdd=()=>{
+const text=inputRef.current.value
+setTodo([...todo,{complited:false,text}])
+// console.log(text)
+inputRef.current.value=""
+
+
+}
+const handleRemove=(index)=>{
+  const newitem=[...todo]
+newitem.splice(index,1)
+  setTodo(newitem)
+}
+const handlelinethrou=(index)=>{
+ const newtoddo=[...todo]
+ newtoddo[index].complited=! newtoddo[index].complited
+ setTodo(newtoddo)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='appContainer'>
+        <div className='container'>
+      <h3>To do list</h3>
+      <ul>
+        {todo.map(({text,complited},index)=>{
+          return(
+            <div key={index} className='mapitem'>
+              <li style={{cursor:"pointer"}} onClick={()=>{handlelinethrou(index)}} className={complited?"childli":""}>{text}</li>
+              <button onClick={()=>{handleRemove(index)}} className='removeli'>X</button>
+            </div>
+          )
+        })}
+      </ul>
+      <input ref={inputRef} placeholder='enter task'/>
+      <button onClick={handleAdd}>Add</button>
     </div>
-  );
+     </div>
+  )
 }
 
-export default App;
+export default App
